d9786266721656db00466251c42efa6317d95629
Jason <vivanaranja@gmail.com>
2018-02-23 20:48:37 -0800

    adding pirate images

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pieces of Eight</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Click each pirate without repeating or walk the plank!
         </p>
       </div>
     );
