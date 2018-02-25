cf952fb18b62f990645dcef5b6bcd10205871e1d
Jason <vivanaranja@gmail.com>
2018-02-24 21:21:49 -0800

    components setup

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+      Pieces of Eight <img alt="ship" src="assets/images/pirateship.svg" />
+    </div>
+  </footer>
+);
+
+export default Footer;
